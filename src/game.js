4dfaefdb2d674ed71815feb62ef8a2a917eca985
mordrax <joseph.ni@gmail.com>
2014-03-17 18:22:48 +1100

    kung fu spear master!

--- src/game.js
+++ src/game.js
@@ -2,23 +2,65 @@
 * Created by mordrax on 17/03/14.
 */
 /// <reference path='../lib/definitions.d.ts'/>
-var game = new Phaser.Game(800, 600, Phaser.CANVAS, 'screen', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
 var keyboard;
-var hero;
+var player;
+var bullets;
 
 function preload() {
-    game.load.image('sky', 'assets/sky.png');
-    game.load.image('ground', 'assets/platform.png');
-    game.load.image('star', 'assets/star.png');
-    game.load.spritesheet('dude', 'assets/player.png', 32, 32);
+    game.load.spritesheet('player', 'assets/player.png', 32, 32);
+    game.load.spritesheet('bullet', 'assets/bullet.png', 32, 32);
 }
 
 function create() {
-    hero = game.add.sprite(0, 0, 'dude');
+    //game.physics.setBoundsToWorld();
+    player = game.add.sprite(0, 0, 'player');
+    game.physics.enable(player, Phaser.Physics.ARCADE);
     keyboard = game.input.keyboard.createCursorKeys();
+    player.body.collideWorldBounds = true;
+    player.body.bounce.setTo(1, 1);
+    player.body.immovable = true;
+    player.anchor.setTo(0.5, 0.5);
+
+    //  Our bullet group
+    bullets = game.add.group();
+    bullets.enableBody = true;
+    bullets.physicsBodyType = Phaser.Physics.ARCADE;
+    bullets.createMultiple(300, 'bullet', 0, false);
+    bullets.setAll('anchor.x', 0.5);
+    bullets.setAll('anchor.y', 0.5);
+    bullets.setAll('outOfBoundsKill', true);
+    bullets.setAll('checkWorldBounds', true);
 }
 
 function update() {
-    game.physics.arcade.moveToPointer(hero, 300, game.input.activePointer);
+    player.body.velocity.setTo(0, 0);
+    if (keyboard.left.isDown) {
+        player.body.velocity.x = -200;
+    }
+    if (keyboard.right.isDown) {
+        player.body.velocity.x = 200;
+    }
+    if (keyboard.up.isDown) {
+        player.body.velocity.y = -200;
+    }
+    if (keyboard.down.isDown) {
+        player.body.velocity.y = 200;
+    }
+
+    player.rotation = game.physics.arcade.angleToPointer(player);
+
+    if (game.input.activePointer.isDown) {
+        //  Boom!
+        fire();
+    }
+}
+
+function fire() {
+    var bullet = bullets.getFirstExists(false);
+    if (!bullet)
+        return;
+    bullet.reset(player.x, player.y);
+    bullet.rotation = game.physics.arcade.moveToPointer(bullet, 1000, game.input.activePointer, 500) + Math.PI / 2;
 }
 //# sourceMappingURL=game.js.map
